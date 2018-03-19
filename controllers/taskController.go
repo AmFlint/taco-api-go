2fd88ff3c59fb81682819abf0679a5da126d764b
AmFlint <amasselot@hotmail.com>
2018-03-19 16:40:20 +0100

    Create Controller for Task Management

---
+++ controllers/taskController.go
@@ -0,0 +1,26 @@
+package controllers
+
+import "net/http"
+
+type TaskController struct {
+
+}
+
+
+func (tc *TaskController) indexAction(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (tc *TaskController) viewAction(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Http Method DELETE on Task resource: Delete a Task
+func (tc *TaskController) deleteAction(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Http Method PUT on Task Resource: Update a Task
+func (tc *TaskController) updateAction(w http.ResponseWriter, r *http.Request) {
+
+}
