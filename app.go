feaf311eee58f1e2353f160d163df5be773ce9de
AmFlint <amasselot@hotmail.com>
2018-03-19 16:39:25 +0100

    [Configuration] Create basic API configuration w/ env var main.go|app.go

---
+++ app.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2"
+	"net/http"
+	"log"
+	"io"
+	"fmt"
+	"github.com/gorilla/handlers"
+	"os"
+)
+
+type App struct {
+	Router    *mux.Router
+	DbSession *mgo.Session
+}
+
+// Variable to contain the database name for later use when Session connects to Database
+var databaseName string
+
+// Initialize Application Structure: Configure Database Connection, save dbName for later uses and create Router
+func (a *App) Initialize(user, password, dbName, dbHost, dbPort string) {
+
+	databaseName = dbName
+	var dbUrl string
+	// Configure dbUrl, manage user/password credentials, if provided
+	if len(user) > 0 && len(password) > 0 {
+		dbUrl += fmt.Sprintf("%s@%s", user, password)
+	}
+	// Add Mongo Host:Port to connection url
+	dbUrl += fmt.Sprintf("%s:%s", dbHost, dbPort)
+
+	fmt.Println(dbUrl)
+
+	var err error
+	// Connect to MongoDb server and retrieve Session Instance
+	a.DbSession, err = mgo.Dial(dbUrl)
+	if err != nil {
+		panic(err)
+	}
+
+	// Initialize Mux Router and assign it to application Structure
+	a.Router = mux.NewRouter()
+}
+
+// Retrieve Database connection from Application's Mongo Session
+func (a *App) Connect() *mgo.Database {
+	return a.DbSession.DB(databaseName)
+}
+
+// Parameter addr of form ":8080", represents the port where the application will be served
+func (a *App) Run(addr string) {
+	// Close Database connection at the end of Application Runtime
+	defer a.DbSession.Close()
+
+	// Routing policies takes place in this function
+	a.setRoutes()
+
+	// Listen on port defined in addr parameter, and serve Application via Mux Router
+	// Configure Http server to log every access/error logs to Stdout
+	if err := http.ListenAndServe(addr, handlers.LoggingHandler( os.Stdout, a.Router)); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Function in charge of setting up Application Routes
+func (a *App) setRoutes() {
+	// ---- General Endpoints ---- //
+
+	// Health Endpoint, check whether service is up or down
+	a.Router.HandleFunc("/health", func(writer http.ResponseWriter, request *http.Request) {
+		defer request.Body.Close()
+		writer.WriteHeader(http.StatusOK)
+		writer.Header().Set("Content-type", "application/json")
+		io.WriteString(writer, `{"alive": true}`)
+	})
+
+	a.Router.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		hello := []byte("Hello, World")
+		w.Write(hello)
+	})
+
+	// ---- Tasks Management Endpoints ---- //
+}
--- main.go
+++ main.go
@@ -1,8 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"fmt"
+)
 
+// Custom Function to manage environment variables and use default values if doesn't exist
+func getenv(key, fallback string) string {
+	value := os.Getenv(key)
+	// If Env Variable not defined, use fallback
+	if len(value) == 0 {
+		value = fallback
+	}
+	return value
+}
 
+// Application entry point, create Application Configuration with Environment Variables, generate Router + DB connection
+// And serve the application
 func main() {
-	fmt.Println("Hello, Taco")
+	a := App{}
+	a.Initialize(
+		getenv("APP_USERNAME", ""),
+		getenv("APP_PASSWORD", ""),
+		getenv("APP_DB_NAME", "taco"),
+		getenv("APP_DB_HOST", "localhost"),
+		getenv("APP_DB_PORT", "27017"))
+
+	// Get Port from Environment Variables and start server (Listen on given Port)
+	port := fmt.Sprintf(":%s", getenv("APP_PORT", "8080"))
+	a.Run(port)
+
 }
