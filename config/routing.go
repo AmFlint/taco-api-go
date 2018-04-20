efb7b523108a06000a215ae4e18a535c49e64423
AmFlint <amasselot@hotmail.com>
2018-04-20 23:13:53 +0200

    [Routing] Duplicate Routes to handle '^/' and '^' paths

--- config/routing.go
+++ config/routing.go
@@ -11,6 +11,7 @@ func (a *App) initializeRoutes() {
 
 	// Health Endpoint, check whether service is up or down
 	a.Router.HandleFunc("/health", routes.HealthIndexHandler).Methods("GET")
+	a.Router.HandleFunc("/health/", routes.HealthIndexHandler).Methods("GET")
 
 	// ---- Tasks Management Endpoints ---- //
 	taskRouter := a.Router.PathPrefix("/boards/{boardId}/tasks").Subrouter()
--- routes/tasks/routes.go
+++ routes/tasks/routes.go
@@ -6,8 +6,16 @@ import (
 
 // Initialize Routes for Task Resource
 func InitRoutes(taskRouter *mux.Router) {
+	// ---- Task Listing ---- //
 	taskRouter.HandleFunc("", TaskIndexHandler).Methods("GET")
+	taskRouter.HandleFunc("/", TaskIndexHandler).Methods("GET")
+	// ----  Task Creation ---- //
 	taskRouter.HandleFunc("", TaskCreateHandler).Methods("POST")
+	taskRouter.HandleFunc("/", TaskCreateHandler).Methods("POST")
+	// ---- Task View  ---- //
 	taskRouter.HandleFunc("/{taskId}", TaskViewHandler).Methods("GET")
+	taskRouter.HandleFunc("/{taskId}/", TaskViewHandler).Methods("GET")
+	// ---- Task Deletion ---- //
 	taskRouter.HandleFunc("/{taskId}", TaskDeleteHandler).Methods("DELETE")
+	taskRouter.HandleFunc("/{taskId}/", TaskDeleteHandler).Methods("DELETE")
 }
