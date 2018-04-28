133cf440d873ebcbaa9c4124eabb68387709548f
AmFlint <amasselot@hotmail.com>
2018-04-28 17:37:11 +0200

    [Constants] Create constants for logger keys

---
+++ constants/logger.go
@@ -0,0 +1,20 @@
+package constants
+
+const (
+	// Misc.
+	ResourceKeyLogger = "resource"
+	HandlerKeyLogger  = "handler"
+	PathKeyLogger = "path"
+	HttpVerbKeyLogger = "http_verb"
+
+	// Handler types logger
+	HandlerCreateLogger = "create"
+	HandlerUpdateLogger = "update"
+	HandlerDeleteLogger = "delete"
+	HandlerViewLogger = "view"
+	HandlerListLogger = "list"
+
+	// Resources
+	ResourceTasksLogger = "tasks"
+	ResourceListsLogger = "lists"
+)
