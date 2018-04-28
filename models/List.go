7c8ecf9a2514cc8c642da2654a68ad25be615a29
AmFlint <amasselot@hotmail.com>
2018-04-28 17:38:33 +0200

    [Models/Lists] Instanciate List with empty array of tasks

--- models/List.go
+++ models/List.go
@@ -5,5 +5,13 @@ import "gopkg.in/mgo.v2/bson"
 type List struct {
 	ListId bson.ObjectId `bson:"_id" json:"listId"`
 	Name   string        `bson:"name" json:"name" onCreate:"nonzero,max=30,regexp=^[a-zA-Z-_ ]*$"`
-	Order  int         `bson:"order" json:"order"`
+	Order  int           `bson:"order" json:"order"`
+	Tasks  []Task        `bson:"tasks" json:"tasks"`
+}
+
+// Initialize List structure with empty array of task
+func NewList() List {
+	list := List{}
+	list.Tasks = []Task{}
+	return list
 }
