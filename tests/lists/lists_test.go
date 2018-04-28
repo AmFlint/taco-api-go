2abe8e9449c628ceec2e0dd30b426b9049192e0d
AmFlint <amasselot@hotmail.com>
2018-04-28 18:34:10 +0200

    [Testing/List] Create tests for List deletion endpoint

--- tests/lists/lists_test.go
+++ tests/lists/lists_test.go
@@ -1,33 +1,36 @@
 package lists
 
 import (
-	"gopkg.in/mgo.v2/bson"
-	"fmt"
-	"github.com/AmFlint/taco-api-go/models"
-	"testing"
-	"net/http"
 	"bytes"
-	"github.com/AmFlint/taco-api-go/helpers"
-	"github.com/AmFlint/taco-api-go/tests/utils"
 	"encoding/json"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/AmFlint/taco-api-go/helpers"
+	"github.com/AmFlint/taco-api-go/models"
+	"github.com/AmFlint/taco-api-go/tests/utils"
 	"github.com/AmFlint/taco-api-go/tests/utils/testconfig"
+	"gopkg.in/mgo.v2/bson"
 )
 
 const (
-	TESTING__LIST_NAME = "Testing list"
+	TESTING__LIST_NAME  = "Testing list"
 	TESTING__LIST_ORDER = 1
 )
+
+// TODO: Create Helpers for Resource creations -> Tests run in parrallell which means reusing an id from above test may not work
 
 func getListsBaseUrl(boardId bson.ObjectId) string {
 	return fmt.Sprintf("/boards/%s/lists/", boardId.Hex())
 }
 
-func getListUrl(boardId, listId bson.ObjectId) string {
-	return fmt.Sprintf("%s/%s/", getListsBaseUrl(boardId), listId.Hex())
+func getlistURL(boardId, listId bson.ObjectId) string {
+	return fmt.Sprintf("%s%s/", getListsBaseUrl(boardId), listId.Hex())
 }
 
-func getInvalidListUrl(boardId bson.ObjectId) string {
-	return fmt.Sprintf("%s/%s/", getListsBaseUrl(boardId), "2")
+func getInvalidlistURL(boardId bson.ObjectId) string {
+	return fmt.Sprintf("%s%s/", getListsBaseUrl(boardId), "2")
 }
 
 // Configuration for basic Lists
@@ -69,10 +72,10 @@ func TestMain(m *testing.M) {
 // ---- Test Create Endpoint ---- //
 func TestCreateListEndpoint(t *testing.T) {
 	t.Run("Create List with valid informations", func(t *testing.T) {
-		listUrl := getListsBaseUrl(boardId)
+		listURL := getListsBaseUrl(boardId)
 		list := getValidList()
 
-		req, _ := http.NewRequest("POST", listUrl, bytes.NewReader(list))
+		req, _ := http.NewRequest("POST", listURL, bytes.NewReader(list))
 		response := utils.ExecuteRequest(req)
 
 		var createdList models.List
@@ -90,20 +93,59 @@ func TestCreateListEndpoint(t *testing.T) {
 	})
 
 	t.Run("Create List with invalid user data - empty title", func(t *testing.T) {
-		listUrl := getListsBaseUrl(boardId)
+		listURL := getListsBaseUrl(boardId)
 		list := getInvalidListEmptyTitle()
-		req, _ := http.NewRequest("POST", listUrl, bytes.NewReader(list))
+		req, _ := http.NewRequest("POST", listURL, bytes.NewReader(list))
 		response := utils.ExecuteRequest(req)
 
 		utils.CheckResponseCode(t, response.Code, http.StatusBadRequest)
 	})
 
 	t.Run("Create List with Invalid Title (bad format)", func(t *testing.T) {
-		listUrl := getListsBaseUrl(boardId)
+		listURL := getListsBaseUrl(boardId)
 		list := getInvalidListBadTitle()
 
-		req, _ := http.NewRequest("POST", listUrl, bytes.NewReader(list))
+		req, _ := http.NewRequest("POST", listURL, bytes.NewReader(list))
 		response := utils.ExecuteRequest(req)
+		utils.CheckResponseCode(t, response.Code, http.StatusBadRequest)
+	})
+}
+
+// ---- Test Delete EndPoint ---- //
+func TestDeleteListHandler(t *testing.T) {
+	t.Run("Delete an Existing task with valid Id", func(t *testing.T) {
+		listURL := getlistURL(boardId, testedListId)
+
+		req, _ := http.NewRequest("DELETE", listURL, nil)
+		response := utils.ExecuteRequest(req)
+
+		utils.CheckResponseCode(t, response.Code, http.StatusOK)
+
+		// Decode the Response
+		var list models.List
+		err := json.Unmarshal(response.Body.Bytes(), &list)
+		if err != nil {
+			t.Error("Could not unmarshal JSON")
+		}
+
+		utils.AssertStringEqualsTo(t, list.Name, TESTING__LIST_NAME)
+	})
+
+	t.Run("Delete a non-existing task with valid ObjectId", func(t *testing.T) {
+		listURL := getlistURL(boardId, bson.NewObjectId())
+
+		req, _ := http.NewRequest("DELETE", listURL, nil)
+		response := utils.ExecuteRequest(req)
+
+		utils.CheckResponseCode(t, response.Code, http.StatusNotFound)
+	})
+
+	t.Run("Delete a task with Invalid Object ID", func(t *testing.T) {
+		listURL := getInvalidlistURL(boardId)
+
+		req, _ := http.NewRequest("DELETE", listURL, nil)
+		response := utils.ExecuteRequest(req)
+
 		utils.CheckResponseCode(t, response.Code, http.StatusBadRequest)
 	})
 }
